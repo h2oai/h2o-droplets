474d14340cbfd0575616a976919ba71fbbc0231b
mmalohlava <michal.malohlava@gmail.com>
2014-12-08 17:05:11 -0800

    Simple project runner.

--- h2o-java-droplet/src/main/java/water/droplets/H2OJavaDroplet.java
+++ h2o-java-droplet/src/main/java/water/droplets/H2OJavaDroplet.java
@@ -17,6 +17,8 @@
 package water.droplets;
 
 import water.DKV;
+import water.H2O;
+import water.H2OApp;
 import water.Iced;
 import water.Key;
 
@@ -30,6 +32,7 @@ public class H2OJavaDroplet {
 
   public static final String MSG = "Hello %s!";
 
+  /** Simple Iced-object which will be serialized over network */
   public static final class StringHolder extends Iced {
     final String msg;
 
@@ -43,12 +46,24 @@ public class H2OJavaDroplet {
     }
   }
 
+  /**
+   * Creates a key and value holding a simple message in {@link water.droplets.H2OJavaDroplet.StringHolder}.
+   *
+   * @return key referencing stored value.
+   */
   public static final Key hello() {
     Key vkey = Key.make("hello.key");
     StringHolder value = new StringHolder(MSG);
     DKV.put(vkey, value);
 
     return vkey;
+  }
+
+  /** Application Entry Point */
+  public static void main(String[] args) {
+	  // Run H2O and build a cloud of 1 member
+	  H2OApp.main(args);
+	  H2O.waitForCloudSize(1, 10*1000 /* ms */);
   }
 }
 

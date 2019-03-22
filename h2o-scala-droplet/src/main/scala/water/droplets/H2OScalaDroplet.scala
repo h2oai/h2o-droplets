6c81ccfc4a3229d37371f50b5005ddde69afebce
Jakub Hava <jakub.hava@gmail.com>
2019-03-22 11:43:13 +0100

    Update H2O Scala droplet & fix issues with compile, test & run

--- h2o-scala-droplet/src/main/scala/water/droplets/H2OScalaDroplet.scala
+++ h2o-scala-droplet/src/main/scala/water/droplets/H2OScalaDroplet.scala
@@ -1,45 +1,63 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package water.droplets
 
-import water.{DKV, Keyed, Key}
+import java.util.concurrent.TimeUnit
+
+import water._
+import water.fvec.NFSFileVec
+import water.parser.ParseDataset
 
 /**
- * H2O Scala bootstrap example.
- *
- * The example implements a tiny Scala library which provides a
- * method putting given greetings message into K/V store.
- */
-object H2OJavaDroplet {
+  * H2O Scala bootstrap example.
+  *
+  * The example implements a tiny Scala library which provides a
+  * method putting given greetings message into K/V store.
+  */
+object H2OScalaDroplet {
 
-  val MSG = "Hello %s!";
+  val MSG = "Hello %s!"
 
-  case class StringHolder(val msg: String) extends Keyed[StringHolder] {
-    def hello(name:String) : String =
+  case class StringHolder(msg: String) extends Keyed[StringHolder] {
+    def hello(name: String): String =
       String.format(msg, name)
 
     override def checksum_impl(): Long = msg.hashCode
   }
 
-  def hello():Key[StringHolder] = {
-    val vkey  = Key.make("hello.key").asInstanceOf[Key[StringHolder]]
-    val value:StringHolder = StringHolder(MSG)
+  def hello(): Key[StringHolder] = {
+    val vkey = Key.make("hello.key").asInstanceOf[Key[StringHolder]]
+    val value: StringHolder = StringHolder(MSG)
     DKV.put(vkey, value)
 
     vkey
+  }
+
+  /** Application Entry Point */
+  def main(args: Array[String]): Unit = { // Run H2O and build a cloud of 1 member
+    H2OApp.main(args)
+    if (H2O.ARGS.client) {
+      H2O.waitForCloudSize(1, TimeUnit.SECONDS.toMillis(10))
+      val key = Key.make("fromJava")
+      val vec = NFSFileVec.make("data/iris.csv")
+      val fr = ParseDataset.parse(key, vec._key)
+      val removed = DKV.get(fr._key)
+      println("Retrieve after removal " + removed)
+    }
   }
 }
--- h2o-scala-droplet/src/test/scala/water/droplets/H2OScalaDropletSpec.scala
+++ h2o-scala-droplet/src/test/scala/water/droplets/H2OScalaDropletSpec.scala
@@ -1,30 +1,34 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package water.droplets
 
-import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
-import water.{H2O, DKV}
-import water.droplets.H2OJavaDroplet.StringHolder
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import water.{DKV, H2O}
+import water.droplets.H2OScalaDroplet.StringHolder
 
 /**
  * The test verify implementation of
- * {@link water.droplets.H2OScalaDroplet} class.
+ * `water.droplets.H2OScalaDroplet` class.
  */
-class H2OJavaDropletSpec extends FlatSpec with Matchers with BeforeAndAfter {
+@RunWith(classOf[JUnitRunner])
+class H2OScalaDropletSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   // Init cloud before run
   before {
@@ -37,7 +41,7 @@ class H2OJavaDropletSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   "H2OScalaDroplet.hello method" should "store a message into K/V" in {
     // Generate hello message and store it in K/V
-    val vkey = H2OJavaDroplet.hello()
+    val vkey = H2OScalaDroplet.hello()
 
     // Get POJO holding hello message
     val helloHolder = DKV.get(vkey).get[StringHolder]()
